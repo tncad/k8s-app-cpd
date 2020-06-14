9970acfb3173fca5aa3ea582af7104b02c8c67b0
tncad <>
2020-06-14 13:14:07 +0200

    improvement idea

--- 011-dockerregistry/pullimages.sh
+++ 011-dockerregistry/pullimages.sh
@@ -13,8 +13,18 @@ for image in `\
   `; 
 do
     echo $image
-    docker pull $image
-    docker tag $image localhost:32000/$image
-    docker push localhost:32000/$image
-    docker rmi $image localhost:32000/$image
+
+    # TODO:
+    # 1. why not pull from local cluster images instead of remote shared repo?
+    # 2. check if image is already available in local repo? 
+    # - check if image is listed by JSON from http://127.0.0.1:32000/v2/<repo_name>/tags/list
+    # - repo_name has to be html encoded using sed 's/\-/\&#45;/g; s/\//\&#47;/g; s/_/&#95;/g;'
+    # - if image is already available localy
+    	# display a friendly message
+    # otherwise
+
+    	docker pull $image
+    	docker tag $image localhost:32000/$image
+    	docker push localhost:32000/$image
+    	#docker rmi $image localhost:32000/$image
 done
