e32ac4f4220962e5c0ee643399b5588c56eb185b
tncad <>
2020-08-05 12:33:05 +0200

    add output value

--- 038-terraform/tf-aws/main.tf
+++ 038-terraform/tf-aws/main.tf
@@ -2,8 +2,11 @@ provider "aws" {
   profile = "default"
   region  = "us-east-1"
 }
-
 resource "aws_instance" "example" {
   ami           = "ami-2757f631"
   instance_type = "t2.micro"
+}
+output "instance_ip_addr" {
+  value       = "${aws_instance.example.*.public_ip}"
+  description = "Pulic IP address of the AWS EC2 instance."
 }
