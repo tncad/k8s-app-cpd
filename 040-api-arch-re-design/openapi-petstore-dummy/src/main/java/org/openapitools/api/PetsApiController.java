4479f48b045282f36630712fce8b35f58f4dcb26
tncad <>
2020-08-17 13:45:39 +0200

    initial commit

---
+++ 040-api-arch-re-design/openapi-petstore-dummy/src/main/java/org/openapitools/api/PetsApiController.java
@@ -0,0 +1,24 @@
+package org.openapitools.api;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.context.request.NativeWebRequest;
+import java.util.Optional;
+@javax.annotation.Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2020-08-17T12:52:46.998893+02:00[Europe/Berlin]")
+@Controller
+@RequestMapping("${openapi.swaggerPetstore.base-path:/v1}")
+public class PetsApiController implements PetsApi {
+
+    private final NativeWebRequest request;
+
+    @org.springframework.beans.factory.annotation.Autowired
+    public PetsApiController(NativeWebRequest request) {
+        this.request = request;
+    }
+
+    @Override
+    public Optional<NativeWebRequest> getRequest() {
+        return Optional.ofNullable(request);
+    }
+
+}
